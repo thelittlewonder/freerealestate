280a47e285f32b7cf11c1c2196bd5a12064d8190
Abhishek Sharma <hello.abhishek@outlook.com>
2020-04-01 19:37:31 +0530

    works for twitter, google, telegram

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -11,13 +11,47 @@ chrome.extension.sendMessage({}, function (response) {
 
 			// Mouse listener for any move event on the current document.
 			document.addEventListener('mousemove', function (e) {
-				var srcElement = e.srcElement;
+				
+				var srcElement = e.target;
+				var allLinks = [];
 
-				// Lets check if our underlying element is a IMG.
-				if (prevDOM != srcElement && srcElement.nodeName == 'A') {
+				//get all the links nested
+				var getNestedLinks = function (node) {
+					if (node.hasChildNodes()) {
+						var child = node.firstChild;
+						if (child.hasChildNodes()) {
+							if (child.nodeName == 'A') {
+								allLinks.push(child);
+							}
+							getNestedLinks(child);
+						} else {
+							child = child.nextSibling;
+						}
+					}
+				}
+				getNestedLinks(srcElement);
 
-					var requestUrl = "https://medium--abhisheksharm22.repl.co/?url=" + srcElement.href;
+				if (prevDOM != srcElement && (srcElement.nodeName == 'A' || allLinks.length>0)) {
+
+					var checkUrl;
+					if (srcElement.nodeName == 'A') {
+						checkUrl = srcElement.href;
+					} else if (allLinks.length > 0) {
+						let tagsSet = new Set(allLinks);
+						checkUrl = [...tagsSet][0].href;
+					}
+					var requestUrl = "https://medium--abhisheksharm22.repl.co/?url=" + checkUrl;
+					
+					//create the box
 					var linkBox = document.createElement('div');
+					var boxTitle = document.createElement('H3');
+					var boxDesc = document.createElement('p');
+					var boxIcon = document.createElement('img');
+					boxIcon.src = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 16 16'%3E%3Cpath fill='%23fff' fill-rule='evenodd' d='M1 0a1 1 0 00-1 1v14c0 .6.4 1 1 1h14c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1H1zm12.3 4.2l-.8.8-.1.2V11.3l1 .8v.2H9v-.2l.9-.8v-5l-2.3 6h-.3l-2.8-6v4l.1.5L5.8 12v.2H2.7V12l1-1.3c.2-.1.3-.3.2-.5V5.7l-.1-.3-1-1.2V4h3l2.5 5.1 2-5.1h3v.2z' clip-rule='evenodd'/%3E%3C/svg%3E";
+					linkBox.appendChild(boxIcon);
+					linkBox.appendChild(boxTitle);
+					linkBox.appendChild(boxDesc);
+					boxDesc.innerHTML = checkUrl.slice(0,40);
 					linkBox.id = 'details';
 
 					// For NPE checking, we check safely. We need to remove the class name
@@ -33,7 +67,8 @@ chrome.extension.sendMessage({}, function (response) {
 					srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
 
 					srcElement.addEventListener('click', async function (e) {
-						e.preventDefault();
+						e.preventDefault();	
+						console.log('clicked: ' + checkUrl)
 						fetch(requestUrl)
 							.then(
 								function (response) {
@@ -42,12 +77,24 @@ chrome.extension.sendMessage({}, function (response) {
 											response.status);
 										return;
 									}
-									
+
 									// Examine the text in the response
 									response.json().then(function (data) {
-										linkBox.innerHTML = JSON.stringify(data.result);
-										console.log(data);
-										document.body.appendChild(linkBox);
+										let result = JSON.stringify(data.result);
+										result = result.slice(1, -1);
+										if(result=='unpaid'){
+											boxTitle.innerHTML='Free Article';
+											linkBox.classList.add('free');
+										} else if(result=='paid'){
+											boxTitle.innerHTML='Paid Article';
+											linkBox.classList.add('paid');
+
+										} else if(result=='not medium'){
+											console.log('Not Medium')
+											boxTitle.innerHTML='Not Medium';
+											linkBox.classList.add('invalid');
+										}
+										srcElement.appendChild(linkBox);
 									});
 								}
 							)
